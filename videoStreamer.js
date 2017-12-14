c67b7e0c8c250bfae3b3f6a943c5a368ab4f41fc
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-12-14 13:13:19 +0330

    Modify Code. Comment Logs

--- videoStreamer.js
+++ videoStreamer.js
@@ -6,19 +6,25 @@ const util = require('util');
 const configs = require('./config.json');
 const router = express.Router();
 
-function generateFileFullLocation (videoFileNameAndFormat) {
+function fetchFileSize(filePath) {
+  let stat = fs.statSync(filePath);
+  return stat.size;
+}
+
+function generateFileFullLocation(videoFileNameAndFormat) {
   return configs.VIDEOS_DIRECTORY + videoFileNameAndFormat;
 }
 
-function generateIndexesWithGivenLength (headerRange, totalSize) {
+function generateIndexesWithGivenLength(headerRange, totalSize) {
   let parts = headerRange.replace(/bytes=/, "").split("-");
   let start = parseInt(parts[0], 10);
   let end = parts[1] ? parseInt(parts[1], 10) : totalSize - 1;
-  console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunkSize);
+  let chunkSize = (end - start) + 1
+  // console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunkSize);
   return {
     start: start,
     end: end,
-    chunkSize: (end - start) + 1
+    chunkSize: chunkSize
   };
 }
 
@@ -26,28 +32,27 @@ function generateIndexesWithGivenLength (headerRange, totalSize) {
 router.get('/:videoName', function (req, res) {
   let videoName = req.params.videoName;
   let path = generateFileFullLocation(videoName);
-
-  let stat = fs.statSync(path);
-  let total = stat.size;
+  let totalSize = fetchFileSize(path);
 
   if (req.headers['range']) {
     let range = req.headers.range;
-    let { start, end, chunkSize} = generateIndexesWithGivenLength(range, total);
+    let { start, end, chunkSize } = generateIndexesWithGivenLength(range, totalSize);
     let file = fs.createReadStream(path, {
       start: start,
       end: end
     });
     res.writeHead(206, {
-      'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + totalSize,
       'Accept-Ranges': 'bytes',
       'Content-Length': chunkSize,
       'Content-Type': 'video/mp4'
     });
     file.pipe(res);
+
   } else {
-    console.log('ALL: ' + total);
+    // console.log('No Request Header - Serving Whole Video With Total Size: ' + totalSize);  
     res.writeHead(200, {
-      'Content-Length': total,
+      'Content-Length': totalSize,
       'Content-Type': 'video/mp4'
     });
     fs.createReadStream(path).pipe(res);
